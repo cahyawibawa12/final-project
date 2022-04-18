3f656d88c32339a29926a832240cb9894a278e84
cahyaw <74125680+cahyawibawa12@users.noreply.github.com>
2022-04-18 11:36:15 +0000

    add validates price 0.01

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,10 @@
 class Menu < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :price, presence:true
+  validates :price, presence:true, numericality: { greater_than_or_equal_to: 0.01 }
+
+
+  def self.by_letter(letter)
+    where("name LIKE ?", "#{letter}%").order(:name)
+  end
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Menu, type: :model do
   it "is valid with a name and a description" do
     menu = Menu.new(
-      name: 'Nasi Goreng Ayam',
-      description: 'Firend rice with chicken',
+      name: "Nasi Goreng Ayam",
+      description: "Fried rice with chicken",
       price: 10000.0
     )
 
@@ -14,7 +14,7 @@ RSpec.describe Menu, type: :model do
   it "is invalid without name" do
     menu = Menu.new(
       name: nil,
-      description: 'Firend rice with chicken',
+      description: "Fried rice with chicken",
       price: 10000.0
     )
     
@@ -25,14 +25,14 @@ RSpec.describe Menu, type: :model do
 
   it "is invalid with a duplicate name" do
     menu1 = Menu.create(
-      name: 'Nasi Goreng Ayam',
-      description: 'Firend rice with chicken',
+      name: "Nasi Goreng Ayam",
+      description: "Fried rice with chicken",
       price: 10000.0
     )
     
     menu2 = Menu.new(
-      name: 'Nasi Goreng Ayam',
-      description: 'Firend rice with chicken',
+      name: "Nasi Goreng Ayam",
+      description: "Fried rice with chicken",
       price: 10000.0
     )
     menu2.valid?
@@ -42,8 +42,8 @@ RSpec.describe Menu, type: :model do
 
   it "is invalid without price" do
     menu = Menu.new(
-      name: 'Nasi Goreng Ayam',
-      description: 'Firend rice with chicken',
+      name: "Nasi Goreng Ayam",
+      description: "Fried rice with chicken",
       price: nil
     )
     
@@ -53,11 +53,20 @@ RSpec.describe Menu, type: :model do
   end
 
   it "is valid with price greater than or equals to 0.01" do
+    menu = Menu.new(
+      name: "Nasi Goreng Ayam",
+      description: "Fried rice with chicken",
+      price: 0.001
+    )
+
+    menu.valid?
+    
+    expect(menu.errors[:price]).to include("must be greater than or equal to 0.01")
   end
 
   it "is invalid without description" do
     menu = Menu.new(
-      name: 'Nasi Goreng Ayam',
+      name: "Nasi Goreng Ayam",
       description: nil,
       price: 10000.0
     )
@@ -65,5 +74,29 @@ RSpec.describe Menu, type: :model do
     menu.valid?
     
     expect(menu.errors[:description]).to include("can't be blank")
+  end
+
+  describe 'self#by_letter' do
+    it "should return a sorted array of results that match" do
+      menu1 = Menu.create(
+        name: "Nasi Goreng Ayam",
+        description: "Fried rice with chicken",
+        price: 10000.0
+      )
+
+      menu2 = Menu.create(
+        name: "Soto Ayam",
+        description: "Traditional Indonesian dish which uses ingredients such as chicken, lontong, noodles, and rice vermicelli.",
+        price: 8000.0
+      )
+
+      menu3 = Menu.create(
+        name: "Nasi Goreng Telor",
+        description: "Fried rice with Egg",
+        price: 8000.0
+      )
+
+      expect(Menu.by_letter("N")).to eq([menu1, menu3])
+    end
   end
 end
